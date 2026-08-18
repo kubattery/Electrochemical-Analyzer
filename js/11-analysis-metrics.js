efb92f41f2e075b591f1c924f98fbb4176d72abc
kubattery <kubattery@gmail.com>
2026-08-18 11:14:05 +0900

    Update 11-analysis-metrics.js

--- js/11-analysis-metrics.js
+++ js/11-analysis-metrics.js
@@ -44,6 +44,9 @@ function runAnalysis() {
 
     // "데이터 한눈에 보기" 통합 뷰가 활성 상태이면 함께 갱신
     if (typeof refreshCombinedIfActive === 'function') refreshCombinedIfActive();
+
+    // Rate/Cycle 탭: 실험 종류 자동 판별 → 뷰 라우팅 + Cyclability 갱신
+    if (typeof refreshCyclabilityIfActive === 'function') refreshCyclabilityIfActive();
 }
 
 /**
